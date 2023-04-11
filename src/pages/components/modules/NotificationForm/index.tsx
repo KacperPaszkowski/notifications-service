9728551ff23bd85e7264652dd19dbb442b66362f
Kacper Paszkowski <kacper.paszkowski@zeiss.com>
2023-04-11 13:40:29 +0200

    notification severity

--- src/pages/components/modules/NotificationForm/index.tsx
+++ src/pages/components/modules/NotificationForm/index.tsx
@@ -4,15 +4,24 @@ import Box from '@mui/material/Box'
 import Paper from '@mui/material/Paper'
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
+import FormControl from '@mui/material/FormControl'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import Select, { SelectChangeEvent } from '@mui/material/Select'
 import TopBar from '../TopBar';
 import axios from "axios";
 import { INotification } from '../../types';
 
 export default function NotificationForm() {
   const [message, setMessage] = useState<string>("");
+  const [notificationType, setNotificationType] = useState<string>("info");
 
   const handleSubmit = async () => {
-    await axios.post("/api/pusher", { type: 'error', message: message });
+    await axios.post("/api/pusher", { type: notificationType, message: message });
+  };
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setNotificationType(event.target.value as string);
   };
 
   return (
@@ -37,6 +46,21 @@ export default function NotificationForm() {
               padding: '30px'
             }}
           >
+            <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Notification type</InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={notificationType}
+                    label="Notification type"
+                    onChange={handleChange}
+                >
+                    <MenuItem value={'error'}>Error</MenuItem>
+                    <MenuItem value={'warning'}>Warning</MenuItem>
+                    <MenuItem value={'info'}>Info</MenuItem>
+                    <MenuItem value={'success'}>Success</MenuItem>
+                </Select>
+            </FormControl>
             <TextField
               label="Message"
               variant="outlined"
--- src/pages/components/modules/NotificationManager/index.tsx
+++ src/pages/components/modules/NotificationManager/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
 import Pusher from "pusher-js";
-import Alert from '@mui/material/Alert'
+import Alert, { AlertColor } from '@mui/material/Alert'
 import styles from './NotificationManager.module.css'
 import { INotification } from '../../types';
 
@@ -27,7 +27,7 @@ function NotificationManager() {
     return ( 
         <div className={styles.notificationContainer}>
             {notifications.map((notification) => (
-                <Alert severity="error">{notification.message} — check it out!</Alert>
+                <Alert severity={notification.type as AlertColor}>{notification.message} — check it out!</Alert>
             ))}
         </div>
      );
