e74987ebc358ecb350d71c5fd103fa36494e0558
KacperPaszkowski <kacper.paszkowskee@gmail.com>
2023-04-12 11:48:08 +0200

    moved dir

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import NotificationForm from './components/modules/NotificationForm';
-import NotificationManager from './components/modules/NotificationManager';
+import NotificationForm from '../components/modules/NotificationForm';
+import NotificationManager from '../components/modules/NotificationManager';
 import { FilterContextProvider } from '@/context/FilterContext';
 
 export default function Home() {
